14e53c104933d1427558e4d33abb200432445626
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2023-06-30 23:55:07 +0600

    cors orgin set a for all

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.use(express.json())
 
 
 // if (process.env.NODE_ENV == "production") {
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 // }
 
 
